6a319b736299b3d07d3ddd438cae1852672b78e5
Aysegul <adundar@purdue.edu>
2014-11-16 09:15:04 -0500

    initial code to replicate network in network

---
+++ data.lua
@@ -0,0 +1,69 @@
+----------------------------------------------------------------------
+-- This script loads the CIFAR10 dataset
+-- training data, and pre-process it to facilitate learning.
+-- Clement Farabet
+----------------------------------------------------------------------
+
+local tar = 'http://data.neuflow.org/data/cifar10.t7.tgz'
+if not paths.dirp('cifar-10-batches-t7') then
+   print '==> downloading dataset'
+   os.execute('wget ' .. tar)
+   os.execute('tar xvf ' .. paths.basename(tar))
+end
+
+-- dataset size: -- will be resized below by opt.smalldata!!!!! be cautious!
+local trsize = 50000
+local tesize = 10000
+
+trainData = {
+   data = torch.Tensor(trsize, 3,32,32),
+   labels = torch.Tensor(trsize),
+   size = function() return trsize end
+}
+
+for i = 0,4 do
+   local subset = torch.load('cifar-10-batches-t7/data_batch_' .. (i+1) .. '.t7', 'ascii')
+   trainData.data[{ {i*10000+1, (i+1)*10000} }] = subset.data:t():float()
+   trainData.labels[{ {i*10000+1, (i+1)*10000} }] = subset.labels:float()
+end
+trainData.labels = trainData.labels + 1
+
+local subset = torch.load('cifar-10-batches-t7/test_batch.t7', 'ascii')
+
+testData = {
+   data = subset.data:t():float(),
+   labels = subset.labels[1]:float(),
+   size = function() return tesize end
+}
+testData.labels = testData.labels + 1
+
+trainData.data = trainData.data:reshape(trsize,3,32,32)
+testData.data = testData.data:reshape(tesize,3,32,32)
+
+print '==> global contrast normalization'
+for i=1, trainData.data:size(1) do
+   local mean = trainData.data[i]:mean()
+   local std = trainData.data[i]:std()
+   trainData.data[i]:add(-mean)
+   trainData.data[i]:div(std)
+end
+for i=1, testData.data:size(1) do
+   local mean = testData.data[i]:mean()
+   local std = testData.data[i]:std()
+   testData.data[i]:add(-mean)
+   testData.data[i]:div(std)
+end
+
+
+----------------------------------------------------------------------
+print '==> whiten data'
+require 'unsup'
+trainData.data, means, P, invP = unsup.zca_whiten(trainData.data)
+testData.data = unsup.zca_whiten(testData.data, means, P, invP)
+
+-- Exports
+return {
+   trainData = trainData,
+   testData = testData,
+}
+
---
+++ train.lua
@@ -0,0 +1,155 @@
+----------------------------------------------------------------------
+-- original taken from Clement
+----------------------------------------------------------------------
+require 'optim'   -- an optimization package, for online and batch methods
+
+----------------------------------------------------------------------
+-- Model + Loss:
+local t = require 'model'
+local model = t.model
+local loss = t.loss
+local dropout = t.dropout
+local lrs = t.lrs
+local wds = t.wds
+
+----------------------------------------------------------------------
+print '==> defining some tools'
+
+-- classes
+local classes = {'1','2','3','4','5','6','7','8','9','0'}
+
+-- This matrix records the current confusion across classes
+local confusion = optim.ConfusionMatrix(classes)
+
+-- Log results to files
+local trainLogger = optim.Logger(paths.concat(opt.save, 'train.log'))
+
+----------------------------------------------------------------------
+print '==> flattening model parameters'
+
+-- Retrieve parameters and gradients:
+-- this extracts and flattens all the trainable parameters of the mode
+-- into a 1-dim vector
+local w,dE_dw = model:getParameters()
+
+local optimState = {
+   learningRate = opt.learningRate,
+   momentum = opt.momentum,
+   damp = 0,
+   weightDecay = opt.weightDecay,
+   learningRateDecay = opt.learningRateDecay,
+   learningRates = lrs,
+   weightDecays = wds
+}
+
+print '==> allocating minibatch memory'
+
+local x = torch.Tensor(opt.batchSize,3,32,32)
+local yt = torch.Tensor(opt.batchSize)
+local target = torch.Tensor(opt.batchSize, 10):fill(0)
+
+if opt.type == 'cuda' then
+   x = x:cuda()
+   yt = yt:cuda()
+   target = target:cuda()
+end
+
+----------------------------------------------------------------------
+print '==> defining training procedure'
+
+local epoch
+
+local function train(trainData)
+
+   -- epoch tracker
+   epoch = epoch or 1
+   local time = sys.clock()
+
+   -- shuffle at each epoch
+   local shuffle = torch.randperm(trainData:size())
+
+   -- do one epoch
+   print('==> doing epoch on training data:')
+   print("==> online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ']')
+   for t = 1,trainData:size(),opt.batchSize do
+      -- disp progress
+      xlua.progress(t, trainData:size())
+      collectgarbage()
+
+      -- batch fits?
+      if (t + opt.batchSize - 1) > trainData:size() then
+         break
+      end
+
+      -- create mini batch
+      local idx = 1
+      target:fill(0)
+      for i = t,t+opt.batchSize-1 do
+         x[idx] = trainData.data[shuffle[i]]
+         yt[idx] = trainData.labels[shuffle[i]]
+         target[idx][trainData.labels[shuffle[i]]] = 1
+         idx = idx + 1
+      end
+      -- create closure to evaluate f(X) and df/dX
+      local eval_E = function(w)
+         -- reset gradients
+         dE_dw:zero()
+         -- evaluate function for complete mini batch
+         local y = model:forward(x)
+         -- estimate df/dW
+         local dE_dy = loss:backward(y,target)
+         model:backward(x,dE_dy)
+
+         return 0,dE_dw
+      end
+     optim.sgd(eval_E, w, optimState)
+
+      -- update confusion
+      -- dropout off
+      for _,d in ipairs(dropout) do
+        d.train = false
+      end
+
+      local y = model:forward(x)
+      for i = 1,opt.batchSize do
+         confusion:add(y[i],yt[i])
+      end
+      -- dropout on
+      for _,d in ipairs(dropout) do
+        d.train = true
+      end
+
+
+   end
+
+   -- time taken
+   time = sys.clock() - time
+   time = time / trainData:size()
+   print("\n==> time to learn 1 sample = " .. (time*1000) .. 'ms')
+
+   -- print confusion matrix
+   print(tostring(confusion))
+
+   -- update logger/plot
+   trainLogger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
+   if opt.plot then
+      trainLogger:style{['% mean class accuracy (train set)'] = '-'}
+      trainLogger:plot()
+   end
+
+   -- save/log current net
+   local filename = paths.concat(opt.save, 'model.net')
+   os.execute('mkdir -p ' .. sys.dirname(filename))
+   print('==> saving model to '..filename)
+   torch.save(filename, model)
+
+   -- next epoch
+   confusion:zero()
+   epoch = epoch + 1
+
+
+end
+
+-- Export:
+return train
+
