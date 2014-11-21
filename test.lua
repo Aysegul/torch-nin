b183d303d583a5dea005e37773324ee75b8eaab5
Aysegul Dundar <adundar@purdue.edu>
2014-11-21 17:36:44 -0500

    plotting option removed

--- test.lua
+++ test.lua
@@ -82,10 +82,6 @@ function test(testData)
 
    -- update log/plot
    testLogger:add{['% mean class accuracy (test set)'] = confusion.totalValid * 100}
-   if opt.plot then
-      testLogger:style{['% mean class accuracy (test set)'] = '-'}
-      testLogger:plot()
-   end
 
    confusion:zero()
 end
