ede8114a71588feb89a4b8ffaeb98c5324d5119a
Aysegul Dundar <adundar@purdue.edu>
2014-12-11 14:47:58 -0500

    white space fix

--- data.lua
+++ data.lua
@@ -6,60 +6,60 @@
 
 local filename = 'cifar-100-preprocessed-gcn-whitened.t7'
 if not paths.dirp(filename) then
-	-- download dataset
-	if not paths.dirp('cifar-10-batches-t7') then
-	   local www = 'http://torch7.s3-website-us-east-1.amazonaws.com/data/cifar-10-torch.tar.gz'
-	   local tar = paths.basename(www)
-	   os.execute('wget ' .. www .. '; '.. 'tar xvf ' .. tar)
-	end
+-- download dataset
+   if not paths.dirp('cifar-10-batches-t7') then
+     local www = 'http://torch7.s3-website-us-east-1.amazonaws.com/data/cifar-10-torch.tar.gz'
+     local tar = paths.basename(www)
+     os.execute('wget ' .. www .. '; '.. 'tar xvf ' .. tar)
+   end
 
-	local trsize = 50000
-	local tesize = 10000
+   local trsize = 50000
+   local tesize = 10000
 
-	trainData = {
-	   data = torch.Tensor(trsize, 3,32,32),
-	   labels = torch.Tensor(trsize),
-	   size = function() return trsize end
-	}
+   trainData = {
+     data = torch.Tensor(trsize, 3,32,32),
+     labels = torch.Tensor(trsize),
+     size = function() return trsize end
+   }
 
-	for i = 0,4 do
-	   local subset = torch.load('cifar-10-batches-t7/data_batch_' .. (i+1) .. '.t7', 'ascii')
-	   trainData.data[{ {i*10000+1, (i+1)*10000} }] = subset.data:t():float()
-	   trainData.labels[{ {i*10000+1, (i+1)*10000} }] = subset.labels:float()
-	end
-	trainData.labels = trainData.labels + 1
+   for i = 0,4 do
+      local subset = torch.load('cifar-10-batches-t7/data_batch_' .. (i+1) .. '.t7', 'ascii')
+      trainData.data[{ {i*10000+1, (i+1)*10000} }] = subset.data:t():float()
+      trainData.labels[{ {i*10000+1, (i+1)*10000} }] = subset.labels:float()
+   end
+   trainData.labels = trainData.labels + 1
 
-	local subset = torch.load('cifar-10-batches-t7/test_batch.t7', 'ascii')
+   local subset = torch.load('cifar-10-batches-t7/test_batch.t7', 'ascii')
 
-	testData = {
-	   data = subset.data:t():float(),
-	   labels = subset.labels[1]:float(),
-	   size = function() return tesize end
-	}
-	testData.labels = testData.labels + 1
+   testData = {
+      data = subset.data:t():float(),
+      labels = subset.labels[1]:float(),
+      size = function() return tesize end
+   }
+   testData.labels = testData.labels + 1
 
-	trainData.data = trainData.data:reshape(trsize,3,32,32)
-	testData.data = testData.data:reshape(tesize,3,32,32)
+   trainData.data = trainData.data:reshape(trsize,3,32,32)
+   testData.data = testData.data:reshape(tesize,3,32,32)
 
-	print '==> gcn data'
-	require 'preprocessing'
-	trainData.data = gcn(trainData.data)
-	testData.data = gcn(testData.data)
+   print '==> gcn data'
+   require 'preprocessing'
+   trainData.data = gcn(trainData.data)
+   testData.data = gcn(testData.data)
 
-	trainData.data = trainData.data:reshape(trsize, 3, 32, 32)
-	testData.data = testData.data:reshape(tesize, 3, 32, 32)
+   trainData.data = trainData.data:reshape(trsize, 3, 32, 32)
+   testData.data = testData.data:reshape(tesize, 3, 32, 32)
 
-	----------------------------------------------------------------------
-	print '==> whiten data'
-	local means, P = zca_whiten_fit(trainData.data)
-	trainData.data = zca_whiten_apply(trainData.data, means, P)
-	testData.data  = zca_whiten_apply(testData.data, means, P)
+   ----------------------------------------------------------------------
+   print '==> whiten data'
+   local means, P = zca_whiten_fit(trainData.data)
+   trainData.data = zca_whiten_apply(trainData.data, means, P)
+   testData.data  = zca_whiten_apply(testData.data, means, P)
 
-	torch.save(filename, {trainData = trainData, testData = testData})
-else 
-	d = torch.load(filename)
-	trainData = d.trainData
-	testData = d.testData
+   torch.save(filename, {trainData = trainData, testData = testData})
+else
+   d = torch.load(filename)
+   trainData = d.trainData
+   testData = d.testData
 end
 
 print(trainData.data:mean())
@@ -69,4 +69,3 @@ return {
    trainData = trainData,
    testData = testData,
 }
-
